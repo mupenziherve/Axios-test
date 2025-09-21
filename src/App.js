8087b5898484822315bcd6648b37dd1a8b704bda
Mupenzi Herve <mupenziherve04@gmail.com>
2025-09-21 13:50:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Title from './components/Title';
+import Setup from './examples/2-headers';
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup/>
+
+    </main>
+  );
+}
+
+export default App;
